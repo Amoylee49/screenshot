6f0d43a812d3261a5e8bfe7071f644c822d088c6
Amoylee49 <Amoyleeo@qq.com>
2023-03-02 01:39:29 +0800

    add README.md 文件

---
+++ README.md
@@ -0,0 +1,29 @@
+# screenshot 网页截图
+
+## [![QQ群](https://img.shields.io/static/v1?label=QQ%E7%BE%A4&message=859357418&color=blue)](https://jq.qq.com/?_wv=1027&k=WtkOwiGX)
+
+基于puppeteer api运行的Node 服务, pm2发布
+
+ ## [GMC地址](https://github.com/ProtobufBot/Go-Mirai-Client/releases)
+
+## 详细说明
+
+运行 npm i puppeteer
+npm i puppeteer-cluster
+
+npm install express
+
+// pm2全局安装
+npm install pm2 -g
+
+
+## 相关文档
+
+- pm2 [详细说明](https://zhuanlan.zhihu.com/p/20940096)
+- pm2  More examples in https://github.com/Unitech/pm2
+
+## CQ WIKI 链接
+     https://wiki.biligame.com/cq/
+
+## 部署相关
+ 配置好 pm2, git 上传Node 项目，
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,28 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
+  apps: [
+    //First Application
+    {
+      name: 'screenshot',
+      script: 'server.js',
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  deploy: {
+    production: {
+      user: 'root',
+      host: '192.168.195.5',
+      ref: 'origin/master',
+      repo: 'https://github.com/Amoylee49/screenshot.git',
+      path: '/root/pm2/screenshot',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
-    }
+    },
+    dev: {}
   }
 };

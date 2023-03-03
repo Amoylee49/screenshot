2410eb775172917b5725e6a9343ad397ba4b15db
Amoylee49 <Amoyleeo@qq.com>
2023-03-03 20:44:56 +0800

    read.me

--- server.js
+++ server.js
@@ -3,24 +3,79 @@ const puppeteer = require('puppeteer')
 const app = express()
 app.get('/puppe', (req, res) => {
     let query = req.query;
-    // let query = eval(req.query);
+    console.log(query)
 
-    // let pic = scrapeYoutube( query);
-console.log(query)
-    const youtubeVideos = new Promise((resolve, reject) => {
-        scrapeYoutube(query)
-            .then(data => {
-                res.type('jpeg')
-                res.send(data)
-            })
-            .catch(
-                err => res.status(500).send("链接访问错误？"))
-    })
-
+    //单次发送  Promise.all()?? https://juejin.cn/post/7003713678419705870
+    loadPage(query)
+        .then(data => {
+            res.type('jpeg')
+            res.send(data[0])
+        })
+        .catch(
+            ereer => {
+                res.status(500).send("链接访问错误？");
+                console.log(ereer);
+            }
+        )
 })
+/**
+ *
+ * @param page
+ * @param selectors 截取的div块
+ * @returns {Promise<{}>}  返回 promise
+ */
+const screenShotDOMElements = async function (page, selectors) {
+    if (!Array.isArray(selectors)) {
+        throw "invalid screenshot selectors";
+    }
 
-const scrapeYoutube = async (query) => {
-    console.log(query.heroName)
+    // let images = { };
+    let images = [];
+    for (let i in selectors) {
+        let selector = selectors[i];
+        if (typeof selector !== 'string') {
+            continue;
+        }
+        //这个page.evaluate返回一个可序列化的普通对象，pageFunction 表示要在页面执行的函数， args 表示传入给 pageFunction 的参数，
+        let rects = await page.evaluate((selector) => {
+            try {
+                let elements = document.querySelectorAll(selector);
+                let ranges = [];
+                elements.forEach(function (element) {
+                    let {left, top, width, height} = element.getBoundingClientRect();
+                    if (width * height !== 0) {
+                        return ranges.push({left, top, width, height})
+                    }
+                });
+                return ranges;
+            } catch (e) {
+                return [];
+            }
+        }, selector);
+
+        // images[selector] = [];
+        for (let j in rects) {
+            let rect = rects[j];
+            let option = {
+                type: 'jpeg',
+                encoding: 'binary',
+                clip: rect ? {
+                    y: rect.top,
+                    x: rect.left,
+                    width: rect.width,
+                    height: rect.height
+                } : null
+            };
+            let imageData = await page.screenshot(option);
+            // images[selector].push(imageData)
+            images.push(imageData)
+        }
+    }
+    console.log(images)
+    return images;
+}
+
+const loadPage = async function (query) {
     const browser = await puppeteer.launch({
             // headless: false,
             // slowMo: 3000,
@@ -30,29 +85,14 @@ const scrapeYoutube = async (query) => {
         }
     )
     const page = await browser.newPage()
-    await page.setViewport({width: 393, height: 180})
-
+    await page.setViewport({width: 611, height: 1228})
     await page.goto('https://wiki.biligame.com/cq/' + query.heroName)
-        .catch(err => {
-            console.log('this error happen',err)
-        });
-
-
-    const title = await page.title()
-    console.log(title)
-    let awaited = await page.$('#mw-content-text > div > div.raw.ibook > div.col-md-5 > div:nth-child(1)');
-    let newVar = await awaited.screenshot({
-        // path: 'form.jpeg',
-        type: 'jpeg',
-        encoding: "binary",
-        quality: 100
-    });
-    // let newVar = await page.$('#hero_skill > div.cqframe_box')
-    // await newVar.screenshot(
-    //     {path: 'form1.png'}
-    // )
-    await browser.close()
-    return newVar
+    // .catch(err => {
+    //     console.log('this error happen',err)
+    // });
+    let selectors = ['#hero_skill', '#hero_skill_sp.cqframe', '#mw-content-text > div > div.raw.ibook > div.col-md-7 > div:nth-child(4)']
+    let promise = screenShotDOMElements(page, selectors);
+    return promise
 }
 
 app.listen(process.env.PORT || 3000)
